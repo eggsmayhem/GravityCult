96bed9e06ae7c5c15d4df411d0c52967e8ee7c63
hunter <flipmybit@protonmail.com>
2022-04-08 00:20:48 +0000

    set up django-heroku/whitenoise

--- sunproject/settings.py
+++ sunproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os 
-
+import django_heroku
 # added to load secret keys
 
 from dotenv import load_dotenv
@@ -161,3 +161,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
