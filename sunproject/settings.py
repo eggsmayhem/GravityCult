61ccfb80786d10e6ad855464487c2d12273800e6
hunter <flipmybit@protonmail.com>
2022-06-21 22:51:54 +0000

    changed STATIC_ROOT again

--- sunproject/settings.py
+++ sunproject/settings.py
@@ -161,6 +161,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 #STATIC_URL = '/static/'
 #STATIC_ROOT = BASE_DIR / 'media'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
 
